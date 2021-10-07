6b161412fd4b52af578a1ea7696014e3a32a29c7
Riddy21 <ridvansong@hotmail.com>
2021-10-07 10:12:23 -0400

    fixing some gitignore issues

--- DCM/gui.py
+++ DCM/gui.py
@@ -7,7 +7,7 @@ class GUI(object):
 
         # Setup window
         self.window.title("DCM")
-        self.window.geometry("500x500")
+        self.window.geometry("600x500")
         self.window.resizable(0, 0)
 
         # Make GUI frame
@@ -20,7 +20,7 @@ class GUI(object):
         self.state = "Welcome"
 
         # Setup menu
-        #self.create_menu()
+        self.create_menu()
 
         # Update loop
         self.update()
@@ -32,7 +32,7 @@ class GUI(object):
 
         # Create a frame and pack with interface
         self.frame = tk.Frame(self.window)
-        title = tk.Label(self.frame, pady=85, text="Welcome to Group 20's pacemaker DCM!")
+        title = tk.Label(self.frame, pady=85, text="Welcome to Group 20's \npacemaker DCM!")
         register = tk.Button(self.frame, text="Create a New Account", padx=220, pady=50, command=lambda: self.main.goto_1p())
         login = tk.Button(self.frame, text='Login to existing account', padx=220, pady=50, command=lambda: self.main.goto_2p())
         close = tk.Button(self.frame, text='Close', padx=230, pady=20, command=lambda: self.main.quit_win())
@@ -41,6 +41,23 @@ class GUI(object):
         login.grid(row=2)
         close.grid(row=3)
         self.frame.pack()
+
+
+    def create_login_screen(self):
+        self.frame = tk.Frame(self.window)
+        title = tk.Label(self.frame, pady=85, text="Login")
+        register = tk.Button(self.frame, text="Create a New Account", padx=220, pady=50, command=lambda: self.main.goto_1p())
+        login = tk.Button(self.frame, text='Login to existing account', padx=220, pady=50, command=lambda: self.main.goto_2p())
+        close = tk.Button(self.frame, text='Close', padx=230, pady=20, command=lambda: self.main.quit_win())
+        title.grid(row=0)
+        register.grid(row=1)
+        login.grid(row=2)
+        close.grid(row=3)
+        self.frame.pack()
+
+    # TODO: Make register screen
+
+    # TODO: Make DCM screen
 
     # Put gui in mainloop
     def loop(self):
