a57d800c692ad93f18f9aef20dc1400b9d5ace69
Riddy21 <ridvansong@hotmail.com>
2021-10-07 10:05:42 -0400

    gui login screen

--- DCM/gui.py
+++ DCM/gui.py
@@ -25,6 +25,23 @@ class GUI(object):
         # Update loop
         self.update()
 
+    # Creates menu GUI
+    def create_menu(self):
+        # console message
+        print("Starting Menu....")
+
+        # Create a frame and pack with interface
+        self.frame = tk.Frame(self.window)
+        title = tk.Label(self.frame, pady=85, text="Welcome to Group 20's pacemaker DCM!")
+        register = tk.Button(self.frame, text="Create a New Account", padx=220, pady=50, command=lambda: self.main.goto_1p())
+        login = tk.Button(self.frame, text='Login to existing account', padx=220, pady=50, command=lambda: self.main.goto_2p())
+        close = tk.Button(self.frame, text='Close', padx=230, pady=20, command=lambda: self.main.quit_win())
+        title.grid(row=0)
+        register.grid(row=1)
+        login.grid(row=2)
+        close.grid(row=3)
+        self.frame.pack()
+
     # Put gui in mainloop
     def loop(self):
         self.window.mainloop()
